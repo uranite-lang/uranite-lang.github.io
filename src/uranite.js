c694b5425fc185e6db91a1bc899d188cecd8e2ae
hxari <hxari@proton.me>
2026-07-30 17:20:38 +0700

    [CREATE] Created src/uranite.js

---
+++ src/uranite.js
@@ -0,0 +1,9 @@
+export const CONFIG = {
+  mainRepo: 'uranite-lang/uranite',
+  apiRepo: 'uranite-lang/docs',
+  githubApi: 'https://api.github.com',
+  rawGithub: 'https://raw.githubusercontent.com',
+  avatarUrl: 'https://avatars.githubusercontent.com/u/294289174?s=200&v=4',
+  cacheTtl: 60 * 60 * 1000,
+  defaultBranch: 'prod'
+}
