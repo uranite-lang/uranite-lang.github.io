eb5312e908a005f94327a44a3d85ea1325f18674
hxari <hxari@proton.me>
2026-07-30 17:20:40 +0700

    [CREATE] Created src/widgets/loading-spinner.vue

---
+++ src/widgets/loading-spinner.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center py-12">
+    <div class="animate-spin rounded-full h-10 w-10 border-4 border-primary-200 dark:border-primary-900 border-t-primary-600"></div>
+  </div>
+</template>
